21242e3f1a230b753e87250d9b5953e39c37e767
RAFAEL DIAS MUNIZ <72029570+rafaeldiasmuniz@users.noreply.github.com>
2022-09-29 17:29:24 -0300

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" type="text/css" href="C:\xampp\htdocs\pasta time\
+    style.css">
+    <?php include "style.css"
+    ?>
+    <title>tela de login</title>
+</head >
+<body>
+    <div class="login">
+        <h1>Login</h1>
+     <input type="text" placeholder="Nome" required/>
+     <br> <br>
+     <input type="text" placeholder="Senha" required/>
+     <br> <br>
+     <button class="button">Entrar</button>
+     <br><br>
+     <button class="button">Cadastrar</button>
+    </body>
+</body>
